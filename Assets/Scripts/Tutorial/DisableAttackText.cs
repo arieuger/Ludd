0ba81f0a6b7bb9cdc0c61ee4e6500dc06d699d8a
A. Roig <antiaroig@gmail.com>
2023-06-18 16:51:19 +0200

    BugFixes

--- Assets/Scripts/Tutorial/DisableAttackText.cs
+++ Assets/Scripts/Tutorial/DisableAttackText.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
 
 public class DisableAttackText : MonoBehaviour {
+
+    private bool canHide = false;
     void Update() {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && canHide)
             GetComponent<LerpAlpha>().isDisabling = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            canHide = true;
+        }
     }
 }
--- Assets/Scripts/Tutorial/DisableDataText.cs
+++ Assets/Scripts/Tutorial/DisableDataText.cs
@@ -2,8 +2,16 @@ using UnityEngine;
 
 public class DisableDataText : MonoBehaviour {
 
+    private bool canHide = false;
+
     void Update() {
-        if (Input.GetButtonDown("Interaction"))
+        if (Input.GetButtonDown("Interaction") && canHide)
             GetComponent<LerpAlpha>().isDisabling = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            canHide = true;
+        }
     }
 }
